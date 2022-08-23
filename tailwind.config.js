df55206e3d25f59080650f9d4d4daaad6ef07cb9
shinzoku <shinzoku62800@gmail.com>
2022-08-23 10:16:44 +0200

    Installation tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        colors: {
+            'myred': '#ef233c',
+            'mywhite': '#edf2f4',
+            'mydarkblue': '#2b2d42',
+            'mygray': '#8d99ae',
+            'gray': {
+                400: '#9CA3AF',
+                500: '#6B7280',
+                600: '#4B5563',
+                700: '#374151',
+                800: '#1F2937',
+                900: '#111827',
+            },
+            'green': {
+                600: '#16A34A',
+                700: '#15803D',
+            },
+        },
+        fontFamily: {
+            // sans: ['Graphik', 'sans-serif'],
+            sans: ['Oleo\\ Script', 'cursive'],
+            serif: ['Merriweather', 'serif'],
+        },
+        extend: {},
+    },
+    plugins: [],
+}
